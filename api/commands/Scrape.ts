b67f0fa06850e496ea322782e4eca1d13b49ee62
Bruce Ingalls <brucei@guo.media>
2020-12-05 23:35:11 -0500

    Scrape to stdout

--- api/commands/Scrape.ts
+++ api/commands/Scrape.ts
@@ -1,4 +1,11 @@
 import { BaseCommand } from '@adonisjs/core/build/standalone'
+// import { Element } from 'nightmare';
+import Nightmare from 'nightmare';
+
+// interface ScrapeElement extends Element {
+//   href: string,
+//   text: string
+// }
 
 export default class Scrape extends BaseCommand {
 
@@ -12,11 +19,35 @@ export default class Scrape extends BaseCommand {
 	 */
   public static description = ''
 
-  public async handle () {
-    this.logger.info('Hello world!')
-  }
+    // Example of recursively extracting text from each list item at
+    // https://lowrey.me/scraping-wikipedia-with-nightmare/
+    public async run () {
+      const url = `https://newyork.craigslist.org/d/computer-gigs/search/cpg`
+      this.logger.info(`Scraping ${url}`)
 
-  public async run () {
-    this.logger.info('Hello world!')
-  }
+      // Set large timeout of 150 seconds, default is 30
+      const nm = new Nightmare({
+        waitTimeout: 150000,
+      });
+
+      // Grab the list from the webpage
+      const list = await nm.goto(url)
+        .wait('body')
+        .evaluate(function() {
+          const items = [];
+          document.querySelectorAll('h3.result-heading > a')
+            // .forEach((el: ScrapeElement) => {
+            .forEach((el) => {
+                items.push({"href": el.href, "text": el.text})
+            });
+          return items;
+        })
+        // .catch(function(error: string) {
+        //   console.error('did not get list', error);
+        // });
+
+        console.dir(list);
+
+      await nm.end();
+    }
 }
